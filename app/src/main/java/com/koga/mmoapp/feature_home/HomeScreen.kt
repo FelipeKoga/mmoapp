e445dbb57eb49f7c9501b3ba12f312bb668f9a43
koga <felipe.koga@aiqfome.com>
2022-07-12 23:51:38 -0300

    feat(games): add games screen

--- app/src/main/java/com/koga/mmoapp/feature_home/HomeScreen.kt
+++ app/src/main/java/com/koga/mmoapp/feature_home/HomeScreen.kt
@@ -1,12 +1,64 @@
 package com.koga.mmoapp.feature_home
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.koga.mmoapp.model.Game
 
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    
+
+    val uiState by viewModel.uiState.collectAsState()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        LazyColumn {
+            items(uiState.games) { game ->
+                GameItem(game)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun GameItem(game: Game) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        AsyncImage(
+            model = game.thumbnail,
+            contentDescription = game.title,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = game.title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+        Text(text = game.description)
+    }
 }
--- app/src/main/java/com/koga/mmoapp/feature_home/HomeViewModel.kt
+++ app/src/main/java/com/koga/mmoapp/feature_home/HomeViewModel.kt
@@ -1,9 +1,43 @@
 package com.koga.mmoapp.feature_home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.koga.mmoapp.data.repository.GameRepository
+import com.koga.mmoapp.data.utils.Resource
+import com.koga.mmoapp.model.Game
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val repository: GameRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            when (val response = repository.fetchGamesStream()) {
+                is Resource.Error -> {
+                    println("#### ${response.throwable}")
+                }
+                is Resource.Success -> {
+                    _uiState.update {
+                        it.copy(games = response.data)
+                    }
+                }
+            }
+
+        }
+    }
 }
+
+data class HomeUiState(
+    val isLoading: Boolean = true,
+    val games: List<Game> = listOf(),
+)
